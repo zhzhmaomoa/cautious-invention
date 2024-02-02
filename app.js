0f76d6e40d8cc14f6f7d4fa90c7ede4aaa505777
liuzhizhen <l742802447@163.com>
2024-02-02 13:28:04 +0800

    upgrade

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path")
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")))
-const port =22345
+const port =443
 app.listen(port,()=>{
-    console.log('http://127.0.0.1:'+22345)
+    console.log('http://127.0.0.1:'+port)
 });
