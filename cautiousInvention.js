e7a6d6a4289d0f31ed99a3d08839f888a2adebcc
liuzhizhen <l742802447@163.com>
2024-03-02 11:25:28 +0800

    upgrade

--- cautiousInvention.js
+++ cautiousInvention.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path")
 const app = express();
 app.use(express.static(path.join(__dirname,"./public")))
-const port = 80
+const port = 59997
 const address = '0.0.0.0';
 app.listen(port,address,()=>{
     console.log('http://'+address+':'+port)
