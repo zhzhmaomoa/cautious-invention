aeb12c979ecefbca90730447df6f0dcbd9c13d12
zhzhl <liuzhizhen@qluanyun.com>
2025-04-25 17:56:51 +0800

    ADD 事件冒泡和捕获

--- src/webPages/knowledge/eventBubbleAndCapture/code.js
+++ src/webPages/knowledge/eventBubbleAndCapture/code.js
@@ -2,12 +2,14 @@ const code1 ={
 	'':{
 		js:`
 function test(e) {
-    div4.innerHTML = \`\${e.target.id}, \${e.currentTarget.id}, \${e.eventPhase}\`
+    div4.insertAdjacentHTML('beforeend',\`<div>\${e.target.id}, \${e.currentTarget.id}, \${e.eventPhase}</div>\`)
+    //event对象中target指触发事件的源头元素，currentTarget指当前触发事件的元素，
+    //eventPhase代表事件类型 1代表事件是捕获触发的 2代表当前对象是源头元素 3代表事件是冒泡触发的
 }
-const bubbleEv = new Event("bubbleEv",{bubbles:true})
+const bubbleEv = new Event("bubbleEv",{bubbles:true})//这个事件是可冒泡到祖先元素使它们触发同一类型事件
 div1.addEventListener("bubbleEv", test)
-div2.addEventListener("bubbleEv", test,{capture: true})
-div3.addEventListener("bubbleEv", test)
+div2.addEventListener("bubbleEv", test,{capture: true})//bubbleEv事件触发时，绑定capture为true的事件的祖先按树的顺序先触发
+div3.addEventListener("bubbleEv", test)//bubbleEv事件触发时，绑定capture为false的事件的祖先按树的倒序后触发
 div4.addEventListener("bubbleEv", test,{capture: true})
 div4.dispatchEvent(bubbleEv)
 `,
@@ -30,9 +32,9 @@ const code2 ={
 <div id="div5">
     <div id="div6">
         <div id="div7">
-        <div id="div8">
+            <div id="div8">
 
-        </div>
+            </div>
         </div>
     </div>
 </div>
@@ -40,9 +42,9 @@ const code2 ={
 		css:'',
 		js:`
 function test(e) {
-    div8.innerHTML = \`\${e.target.id}, \${e.currentTarget.id}, \${e.eventPhase}\`
+    div8.insertAdjacentHTML('beforeend',\`<div>\${e.target.id}, \${e.currentTarget.id}, \${e.eventPhase}</div>\`)
 }
-const ev =new Event("ev")
+const ev =new Event("ev")//如果是不冒泡的事件，则不会倒序触发祖先事件
 div5.addEventListener("ev", test)
 div6.addEventListener("ev", test,{capture: true})
 div7.addEventListener("ev", test)
