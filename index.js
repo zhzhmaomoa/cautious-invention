00410d1c5f042cba3f5c377e345ddf3287714869
liuzhizhen <l742802447@163.com>
2026-01-12 11:14:53 +0800

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path")
 const app = express();
 app.use(express.static(path.join(__dirname,"./src")))
 const port = 59997
-const address = '0.0.0.0';
+const address = '127.0.0.1';
 app.listen(port,address,()=>{
     console.log('http://'+address+':'+port)
 });
